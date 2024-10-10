9ab79e4f4fad0cf3b60a767ad95151e091ecf4d0
Jean Carlos Santana <jsantanaortiz1701@gmail.com>
2024-10-09 22:06:57 -0400

    Creacion de los DTO(Data Transfer object)

---
+++ Registro de Personas/DTO/ActualizarPersonaDto.cs
@@ -0,0 +1,27 @@
+﻿namespace Registro_de_Personas.DTO
+{
+    public class ActualizarPersonaDto
+    {
+        public string Nombre { get; set; } = null!;
+
+        public string Apellido { get; set; } = null!;
+
+        public string Cedula { get; set; } = null!;
+
+        public string Direccion { get; set; } = null!;
+
+        public string Telefono { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public DateOnly FechaNacimiento { get; set; }
+
+        public string Genero { get; set; } = null!;
+
+        public string EstadoCivil { get; set; } = null!;
+
+        public string Nacionalidad { get; set; } = null!;
+
+        public int Foto { get; set; }
+    }
+}
---
+++ Registro de Personas/DTO/PersonasDto.cs
@@ -0,0 +1,31 @@
+﻿namespace Registro_de_Personas.DTO
+{
+    public class PersonasDto
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; } = null!;
+
+        public string Apellido { get; set; } = null!;
+
+        public string Cedula { get; set; } = null!;
+
+        public string Direccion { get; set; } = null!;
+
+        public string Telefono { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public DateOnly FechaNacimiento { get; set; }
+
+        public string Genero { get; set; } = null!;
+
+        public string EstadoCivil { get; set; } = null!;
+
+        public string Nacionalidad { get; set; } = null!;
+
+        public DateTime? FechaDeReguistro { get; set; }
+
+        public int Foto { get; set; }
+    }
+}
