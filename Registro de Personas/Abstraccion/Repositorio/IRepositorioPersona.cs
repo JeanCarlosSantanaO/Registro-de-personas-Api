094dc37339fba911d3338d9544d7724086daaf38
Jean Carlos Santana <jsantanaortiz1701@gmail.com>
2024-10-09 22:03:40 -0400

    Creacion de la abstraccion

---
+++ Registro de Personas/Abstraccion/Repositorio/IRepositorioPersona.cs
@@ -0,0 +1,15 @@
+﻿using Registro_de_Personas.DTO;
+using Registro_de_Personas.Modelos;
+
+namespace Registro_de_Personas.Abstraccion.Repositorio
+{
+    public class IRepositorioPersona
+    {
+        List<Persona> Get();
+        Persona GetById(int id);
+        Persona Create(CrearPersonaDtocs crearPersonaDtocs);
+        Persona update(ActualizarPersonaDto actualizarPersonaDto);
+        void Delete(int id);
+
+    }
+}
---
+++ Registro de Personas/Abstraccion/Servicios/IServiciosPersonas.cs
@@ -0,0 +1,14 @@
+﻿using Registro_de_Personas.DTO;
+using Registro_de_Personas.Modelos;
+
+namespace Registro_de_Personas.Abstraccion.Servicios
+{
+    public interface IServiciosPersonas
+    {
+        List<PersonasDto> Get();
+        Persona GetById(int id);
+        PersonasDto Create(CrearPersonaDtocs crearPersonaDtocs);
+        PersonasDto Update(int id,ActualizarPersonaDto actualizarPersonaDto);
+        void Delete(int id);
+    }
+}
