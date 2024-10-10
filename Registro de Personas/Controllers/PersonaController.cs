8941b51ef36377dce99b412e64c9d7bf79c63698
Jean Carlos Santana <jsantanaortiz1701@gmail.com>
2024-10-09 22:05:32 -0400

    Creacion del controlador

---
+++ Registro de Personas/Controllers/PersonaController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Registro_de_Personas.Abstraccion.Servicios;
+using Registro_de_Personas.DTO;
+
+namespace Registro_de_Personas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonaController : ControllerBase
+    {
+        private readonly IServiciosPersonas serviciosPersonas;
+        public PersonaController(IServiciosPersonas servicios)
+        {
+            serviciosPersonas = servicios;
+       
+        }
+
+        [HttpGet]
+        public IActionResult Get() 
+        {
+            var result = serviciosPersonas.Get();
+            return Ok(result);
+        }
+        [HttpPost]
+        public IActionResult Create(CrearPersonaDtocs crearPersonaDtocs)
+        {
+            var result = serviciosPersonas.Create(crearPersonaDtocs);
+            return Ok(result);
+        }
+        [HttpPut("{id}")]
+        public IActionResult Update(int id,ActualizarPersonaDto actualizar)
+        {
+            var result = serviciosPersonas.Update(id, actualizar);
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id) 
+        {
+            serviciosPersonas.Delete(id);
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var result = serviciosPersonas.GetById(id);
+            return Ok(result);
+        }
+
+    }
+
+    
+}
