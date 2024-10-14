62c53eb1a842875bbdd400164b3eca0b36ce8335
Jean Carlos Santana <jsantanaortiz1701@gmail.com>
2024-10-13 23:29:55 -0400

    Creamos la implementacion

---
+++ Registro de Personas/Implementaciones/Repositorio/RepositorioPersona.cs
@@ -0,0 +1,92 @@
+﻿using Registro_de_Personas.Abstraccion.Repositorio;
+using Registro_de_Personas.DTO;
+using Registro_de_Personas.Modelos;
+
+namespace Registro_de_Personas.Implementaciones.Repositorio
+{
+    public class RepositorioPersona : IRepositorioPersona
+    {
+        private readonly PersonasContext _context;
+
+        public RepositorioPersona(PersonasContext context)
+        {
+            _context = context;
+        }
+
+
+        public Persona Create(CrearPersonaDtocs crearPersonaDtocs)
+        {
+            var persona = new Persona
+            {
+                Nombre = crearPersonaDtocs.Nombre,
+                Apellido = crearPersonaDtocs.Apellido,
+                Cedula = crearPersonaDtocs.Cedula,
+                Direccion = crearPersonaDtocs.Direccion,
+                Telefono = crearPersonaDtocs.Telefono,
+                Email = crearPersonaDtocs.Email,
+                FechaNacimiento = crearPersonaDtocs.FechaNacimiento,
+                Genero = crearPersonaDtocs.Genero,
+                EstadoCivil = crearPersonaDtocs.EstadoCivil,
+                Nacionalidad = crearPersonaDtocs.Nacionalidad,
+                Foto = crearPersonaDtocs.Foto
+            }; 
+            _context.Personas.Add(persona);
+            _context.SaveChanges();
+            return persona;
+        }
+
+        public Persona Update(int id,ActualizarPersonaDto actualizarPersonaDto) 
+        {
+            var personasExistente = GetById(id);
+
+
+            personasExistente.Nombre = actualizarPersonaDto.Nombre ?? personasExistente.Nombre;
+            personasExistente.Apellido = actualizarPersonaDto.Apellido ??personasExistente.Apellido;
+            personasExistente.Cedula = actualizarPersonaDto.Cedula ?? personasExistente.Cedula;
+            personasExistente.Direccion = actualizarPersonaDto.Direccion ?? personasExistente.Direccion;
+            personasExistente.Telefono = actualizarPersonaDto.Telefono ?? personasExistente.Telefono;
+            personasExistente.Email = actualizarPersonaDto.Email ?? personasExistente.Email;
+            personasExistente.FechaNacimiento = actualizarPersonaDto.FechaNacimiento ?? personasExistente.FechaNacimiento;
+            personasExistente.Genero = actualizarPersonaDto.Genero ?? personasExistente.Genero;
+            personasExistente.EstadoCivil = actualizarPersonaDto.EstadoCivil ?? personasExistente.EstadoCivil;
+            personasExistente.Nacionalidad = actualizarPersonaDto.Nacionalidad ?? personasExistente.Nacionalidad;
+            personasExistente.Foto = actualizarPersonaDto.Foto ?? personasExistente.Foto;
+            
+            _context.Update(personasExistente);
+            _context.SaveChanges();
+            var personaActualizada = GetById(id);
+            return personaActualizada;
+           
+
+        }
+
+
+      public List<Persona> Get()
+      {
+            return [.. _context.Personas];
+      }
+
+        public Persona GetById(int id) 
+        {
+            return _context.Personas.Where(p => p.Id==id).FirstOrDefault();
+        }
+
+
+        public void Delete(int id)
+        {
+            Persona persona= GetById(id);
+            _context.Remove(persona);
+            _context.SaveChanges();
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Registro de Personas/Implementaciones/Servicios/ServiciosPersonas.cs
@@ -0,0 +1,106 @@
+﻿using Registro_de_Personas.Abstraccion.Repositorio;
+using Registro_de_Personas.Abstraccion.Servicios;
+using Registro_de_Personas.DTO;
+using Registro_de_Personas.Implementaciones.Repositorio;
+using Registro_de_Personas.Modelos;
+
+namespace Registro_de_Personas.Implementaciones.Servicios
+{
+    public class ServiciosPersonas :IServiciosPersonas
+    {
+        private readonly IRepositorioPersona repositorioPersona;
+        
+        public ServiciosPersonas(IRepositorioPersona repositorio)
+        {
+            repositorioPersona=repositorio;
+
+        }
+
+        public List<PersonasDto> Get()
+        {
+            var personas = repositorioPersona.Get();
+            var personasDto = new List<PersonasDto>();
+            foreach (Persona persona in personas)
+            {
+                var npersonaDto = new PersonasDto()
+                {
+                    Id = persona.Id,
+                    Nombre = persona.Nombre,
+                    Apellido = persona.Apellido,
+                    Cedula = persona.Cedula,
+                    Direccion = persona.Direccion,
+                    Telefono = persona.Telefono,
+                    Email = persona.Email,
+                    EstadoCivil = persona.EstadoCivil,
+                    Nacionalidad = persona.Nacionalidad,
+                    Foto = persona.Foto,
+                    FechaNacimiento = persona.FechaNacimiento,
+                    FechaDeReguistro = persona.FechaDeReguistro,
+                    Genero = persona.Genero
+
+                };
+                
+                personasDto.Add(npersonaDto);
+            }
+            return personasDto;
+        }
+
+        public Persona GetById(int id)
+        {
+            return repositorioPersona.GetById(id);
+        }
+
+
+        public PersonasDto Create(CrearPersonaDtocs crearPersonaDtocs)
+        {
+            var personas = repositorioPersona.Create(crearPersonaDtocs);
+            var personasDto = new PersonasDto
+            {
+                Id = personas.Id,
+                Nombre = personas.Nombre,
+                Apellido = personas.Apellido,
+                Cedula = personas.Cedula,
+                Direccion = personas.Direccion,
+                Telefono = personas.Telefono,
+                Email = personas.Email,
+                EstadoCivil = personas.EstadoCivil,
+                Nacionalidad = personas.Nacionalidad,
+                Foto = personas.Foto,
+                FechaNacimiento = personas.FechaNacimiento,
+                FechaDeReguistro = personas.FechaDeReguistro,
+                Genero = personas.Genero
+            };
+            return personasDto;
+
+
+        }
+
+        public PersonasDto Update(int id, ActualizarPersonaDto actualizarPersonaDto)
+        {
+            var persona = repositorioPersona.Update(id, actualizarPersonaDto);
+            var personaDTO = new PersonasDto
+            {
+
+                Id =id,
+                Nombre = persona.Nombre,
+                Apellido = persona.Apellido,
+                Cedula = persona.Cedula,
+                Direccion = persona.Direccion,
+                Telefono = persona.Telefono,
+                Email = persona.Email,
+                EstadoCivil = persona.EstadoCivil,
+                Nacionalidad = persona.Nacionalidad,
+                Foto = persona.Foto,
+                FechaNacimiento = persona.FechaNacimiento,
+                FechaDeReguistro = persona.FechaDeReguistro,
+                Genero = persona.Genero
+            };
+            return personaDTO;
+        }
+
+        public void Delete(int id)
+        {
+           repositorioPersona.Delete(id);
+        }
+    }
+}
