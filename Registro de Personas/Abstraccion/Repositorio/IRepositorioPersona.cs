0e171378fd80163006e8f982c822c5359b5e9d3e
Jean Carlos Santana <jsantanaortiz1701@gmail.com>
2024-10-13 23:28:48 -0400

    Cambiamos el tipo de clase

--- Registro de Personas/Abstraccion/Repositorio/IRepositorioPersona.cs
+++ Registro de Personas/Abstraccion/Repositorio/IRepositorioPersona.cs
@@ -3,12 +3,12 @@ using Registro_de_Personas.Modelos;
 
 namespace Registro_de_Personas.Abstraccion.Repositorio
 {
-    public class IRepositorioPersona
+    public interface IRepositorioPersona
     {
         List<Persona> Get();
         Persona GetById(int id);
         Persona Create(CrearPersonaDtocs crearPersonaDtocs);
-        Persona update(ActualizarPersonaDto actualizarPersonaDto);
+        Persona Update(int id,ActualizarPersonaDto actualizarPersonaDto);
         void Delete(int id);
 
     }
